1c665e91dd8afaba27f1740a60850e0e0d40c34e
caitlin <caitlin.wink@gmail.com>
2021-04-03 15:37:04 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Add your code here
+// function submitData(name, email) {
+//     fetch("http://localhost:3000/users", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify({
+//     name: "Steve",
+//     email: "steve@steve.com"
+//   })
+// }); 
+// }
+
+function submitData(name, email) {
+fetch("http://localhost:3000/users", {
+    method: "POST" , 
+    headers: {
+        "Content-Type": "application/json" ,
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        name: "Steve",
+        email: "steve@steve.com"
+      })
+})
+.then(response => response.json())
+.then(data => {
+    console.log("Success:", data)
+})
+.catch((error) => {
+    console.error("Error:", error);
+})
+}
+debugger

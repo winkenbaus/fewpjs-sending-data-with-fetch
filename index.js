be46eb6dfdd6925333e1f9558d357aaec41f5726
caitlin <caitlin.wink@gmail.com>
2021-04-05 10:56:58 -0700

    commit tree 111c9dfcddba4fe9e416c6c3ae23519e9a4a9273

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
-// function submitData(name, email) {
-//     fetch("http://localhost:3000/users", {
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json",
-//     "Accept": "application/json"
-//   },
-//   body: JSON.stringify({
-//     name: "Steve",
-//     email: "steve@steve.com"
-//   })
-// }); 
-// }
+
 
 function submitData(name, email) {
-fetch("http://localhost:3000/users", {
-    method: "POST" , 
-    headers: {
-        "Content-Type": "application/json" ,
-        "Accept": "application/json"
-    },
-    body: JSON.stringify({
-        name: "Steve",
-        email: "steve@steve.com"
-      })
-})
-.then(response => response.json())
-.then(data => {
-    console.log("Success:", data)
-})
-.catch((error) => {
-    console.error("Error:", error);
-})
+    fetch("http://localhost:3000/users", {
+        method: "POST" , 
+        headers: {
+            "Content-Type": "application/json" ,
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: "Steve",
+            email: "steve@steve.com"
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        renderData(data)
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    })
 }
-debugger
+
+function renderData(user) {
+    const name = user.name
+    const email = user.email
+    const id = user.id
+    const h1 = document.createElement("h1")
+    h1.innerText = (name + " " + email + " " + id)
+    document.body.appendChild(h1)   
+}
